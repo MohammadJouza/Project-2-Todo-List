49c2db5a095c3ad22c65aae305dabd36c6a03e66
Mohammad Jouza <m.jozaa@yahoo.com>
2021-12-19 12:04:28 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,39 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const app = express();
 
-const db=require('./db')
-const Todo=require('./todo')
+
+const db = require("./db");
+const Todo = require("./todo");
 // console.log(Todo);
 
-app.get('/',(req,res)=>{
-  res.json('GET / is Working')
-})
+app.use(express.json())
 
-app.get('/tasks',(req,res)=>{
-  res.json('GET / is Working')
-})
+app.get("/", (req, res) => {
+  res.json("GET / is Working");
+});
 
-app.listen(5000,()=>{
-  console.log('SERVER IS WORKING ..');
-})
+app.get("/tasks", (req, res) => {
+  Todo.find({}, (err, data) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
+app.post("/tasks", (req, res) => {
+  console.log('25:',req.body);
 
+  Todo.create(req.body, (err, newTask) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+      res.status(201).json(newTask);
+    }
+  });
+});
 
+app.listen(5000, () => {
+  console.log("SERVER IS WORKING ..");
+});
