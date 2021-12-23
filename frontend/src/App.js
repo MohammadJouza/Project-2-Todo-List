377ab4aff58d18e0766365b36b8303ecef9f7627
Mohammad Jouza <m.jozaa@yahoo.com>
2021-12-23 10:16:15 +0300

    finish get pending and done, delete done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -73,9 +73,38 @@ export default function App() {
       });
   };
 
+  const deleteTasks = () => {
+    axios
+      .delete(`http://localhost:5000/tasks`)
+      //     (`http://localhost:5000/tasks/${id}`)
+      .then((response) => {
+        // console.log('RESPONSE: ', response);
+        console.log("DATA: ", response.data);
+        getData();
+        // change react hooks state using spread operator
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+  };
+
+  const filterData = (status) => {
+    // should bring data using axios
+    // from backend (GET /tasks)
+    axios
+      .get(`http://localhost:5000/filter?isCompleted=${status}`)
+      .then((response) => {
+        // console.log('RESPONSE: ', response);
+        console.log("DATA: ", response.data);
+        setTasks(response.data);
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+  };
   const mapOverTasks = tasks.map((taskObj, i) => (
     <Todo
-      key={i}
+      key={taskObj._id}
       task={taskObj}
       deleteTodo={deleteTodo}
       toggleTodo={toggleTodo}
@@ -86,6 +115,22 @@ export default function App() {
       <p>app</p>
       {/* click on button should bring all Data */}
       <button onClick={getData}>GET TASKS</button>
+      <button onClick={deleteTasks}>DELETE Completed tasks </button>
+      <button
+        onClick={() => {
+          filterData(true);
+        }}
+      >
+        GET DONE
+      </button>
+      <button
+        onClick={() => {
+          filterData(false);
+        }}
+      >
+        GET PENDING
+      </button>
+
       <Add createFunc={postNewTodo} />
       {mapOverTasks}
     </div>
